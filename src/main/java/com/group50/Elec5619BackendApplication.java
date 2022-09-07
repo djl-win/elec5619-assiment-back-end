d3d3c9ece9e3cf0c49db0061355361841eedb443
jialeWin <day.dong99@yahoo.com>
2022-09-07 22:19:58 +1000

    init

---
+++ src/main/java/com/group50/Elec5619BackendApplication.java
@@ -0,0 +1,13 @@
+package com.group50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elec5619BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Elec5619BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group50/Elec5619BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elec5619BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
